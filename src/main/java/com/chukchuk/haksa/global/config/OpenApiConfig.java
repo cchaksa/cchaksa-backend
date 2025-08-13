14786ac6d40308f4134e8873d2b282d3203d7dcf
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-08-13 11:21:50 +0900

    fix: swagger 설정 변경

--- src/main/java/com/chukchuk/haksa/global/config/OpenApiConfig.java
+++ src/main/java/com/chukchuk/haksa/global/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement; // ★ 추가
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,22 +22,21 @@ import java.util.List;
 )
 public class OpenApiConfig {
 
-        @Value("${swagger.server-url}")
-        private String serverUrl;
+    @Value("${swagger.server-url}")
+    private String serverUrl;
 
-        @Value("${spring.profiles.active:default}")
-        private String activeProfile;
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
 
-        @Bean
-        public OpenAPI customOpenAPI() {
-                Server server = new Server()
-                        .url(serverUrl)
-                        .description(activeProfile.substring(0, 1).toUpperCase() + activeProfile.substring(1) + " Server");
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server()
+                .url(serverUrl)
+                .description(Character.toUpperCase(activeProfile.charAt(0)) + activeProfile.substring(1) + " Server");
 
-                return new OpenAPI()
-                        .info(new Info().title("척척학사 API")
-                                .version("v1")
-                                .description("API 명세서"))
-                        .servers(List.of(server));
-        }
+        return new OpenAPI()
+                .info(new Info().title("척척학사 API").version("v1").description("API 명세서"))
+                .servers(List.of(server))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
