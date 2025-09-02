b150f5b7f7db1886ab4db9420423451cd8cf2371
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-09-02 12:11:08 +0900

    remove: 불필요한 ApiError 클래스 제거

--- src/main/java/com/chukchuk/haksa/global/logging/web/ApiError.java
+++
@@ -1,7 +0,0 @@
-package com.chukchuk.haksa.global.logging.web;
-
-public record ApiError(String code, String message) {
-    public static ApiError of(String code, String message) {
-        return new ApiError(code, message);
-    }
-}
