cb09eaad3019605f84f12f498e18ddaa50ed6cc0
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-09-15 11:14:50 +0900

    feat: 2025 입학자 특수 학과 졸업요건 플래그(hasDifferentGraduationRequirement) 추가

--- src/main/java/com/chukchuk/haksa/domain/graduation/dto/GraduationProgressResponse.java
+++ src/main/java/com/chukchuk/haksa/domain/graduation/dto/GraduationProgressResponse.java
@@ -12,4 +12,15 @@ import java.util.List;
 public class GraduationProgressResponse {
     @Schema(description = "졸업 요건 영역별 이수 현황", required = true)
     private List<AreaProgressDto> graduationProgress;
+
+    @Schema(description = "특정 학과/연도 예외로 기존과 다른 졸업요건이 적용되는지 여부", required = true)
+    private boolean hasDifferentGraduationRequirement = false;
+
+    public GraduationProgressResponse(List<AreaProgressDto> graduationProgress) {
+        this.graduationProgress = graduationProgress;
+    }
+
+    public void setHasDifferentGraduationRequirement() {
+        this.hasDifferentGraduationRequirement = true;
+    }
 }
