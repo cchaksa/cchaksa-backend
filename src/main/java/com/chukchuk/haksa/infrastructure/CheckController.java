539fca17d9dd4dfb2b2a95e8efa247ddb5a8ae99
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-12-22 16:51:22 +0900

    126 feat: Sentry 상태 체크를 위한 endpoint 추가

--- src/main/java/com/chukchuk/haksa/infrastructure/CheckController.java
+++ src/main/java/com/chukchuk/haksa/infrastructure/CheckController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
-public class HealthCheckController {
+@RequestMapping
+public class CheckController {
 
-    @GetMapping
+    @GetMapping("/health")
     public String health() {
         return "ok";
+    }
+
+    @GetMapping("/sentry-test")
+    public void sentryTest() {
+        throw new RuntimeException("SENTRY_TEST_DEV");
     }
 }
