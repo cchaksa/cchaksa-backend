1b4adba4a87b8ff81f596754ff3436ec21c33496
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-01-27 01:47:23 +0900

    158 refactor: 졸업 요건 조회 후보 탐색 개선

--- src/main/java/com/chukchuk/haksa/domain/department/repository/DepartmentRepository.java
+++ src/main/java/com/chukchuk/haksa/domain/department/repository/DepartmentRepository.java
@@ -3,8 +3,10 @@ package com.chukchuk.haksa.domain.department.repository;
 import com.chukchuk.haksa.domain.department.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByDepartmentCode(String departmentCode);  // departmentCode로 학과 찾기
+    List<Department> findAllByEstablishedDepartmentName(String establishedDepartmentName);
 }
